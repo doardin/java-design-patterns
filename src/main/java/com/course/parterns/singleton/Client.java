e9f8511c1d901436f2d05859c01c48bbbb80f782
Eduardo Lopes <duh.lopes234@hotmail.com>
2022-04-06 01:42:29 -0300

    feat: Singleton

---
+++ src/main/java/com/course/parterns/singleton/Client.java
@@ -0,0 +1,41 @@
+package com.course.parterns.singleton;
+
+import com.course.parterns.singleton.conn.Connection;
+import com.course.parterns.singleton.conn.ConnectionPool;
+
+public class Client {
+    public static void doQuery1(){
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection conn = pool.getConnection();
+        if(conn != null){
+            conn.query("SELECT * FROM A1");
+        }
+        pool.leaveConnection(conn);
+    }
+
+    public static void doQuery2(){
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection conn = pool.getConnection();
+        if(conn != null){
+            conn.query("SELECT * FROM A2");
+        }
+        pool.leaveConnection(conn);
+    }
+
+    public static void doQuery3(){
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection conn = pool.getConnection();
+        if(conn != null){
+            conn.query("SELECT * FROM A3");
+        }
+        pool.leaveConnection(conn);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Connection Pool Size: " + ConnectionPool.POOL_SIZE);
+        doQuery1();
+        doQuery2();
+        doQuery3();
+    }
+
+}
